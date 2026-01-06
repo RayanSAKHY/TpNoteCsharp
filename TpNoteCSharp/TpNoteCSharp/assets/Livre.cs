1c59d22abbef884ac3a3343f0666dd9d1e72638d
Windowsrsakhy <rayansakhy9@gmail.com>
2026-01-06 11:01:24 +0100

    Implementation minime d'une classe livre

---
+++ TpNoteCSharp/TpNoteCSharp/assets/Livre.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TpNoteCSharp.assets
+{
+    internal class Livre
+    {
+        private string _titre;
+        private string _auteur;
+        private DateTime _anneePublication;
+        private int _isbn;
+        private string _categorie;
+        private DateTime _dateAjout;
+
+        public Livre(string titre, string auteur, DateTime anneePublication, int isbn, string categorie, DateTime dateAjout)
+        {
+            _titre = titre;
+            _auteur = auteur;
+            _anneePublication = anneePublication;
+            _isbn = isbn;
+            _categorie = categorie;
+            _dateAjout = dateAjout;
+        }
+
+        public string Titre
+        {
+            get { return _titre; }
+            set { _titre = value; }
+        }
+
+        public string Auteur
+        {
+            get { return _auteur; }
+            set { _auteur = value; }
+        }
+
+        public DateTime AnneePublication
+        {
+            get { return _anneePublication; }
+            set { _anneePublication = value; }
+        }
+
+        public int ISBN
+        {
+            get { return _isbn; }
+            set { _isbn = value; }
+        }
+
+        public string Categorie
+        {
+            get { return _categorie; }
+            set { _categorie = value; }
+        }
+
+        public DateTime DateAjout
+        {
+            get { return _dateAjout; }
+            set { _dateAjout = value; }
+        }
+
+    }
+}
