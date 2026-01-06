2492180cf827ad35153fb09b99f3c69108332e31
Windowsrsakhy <rayansakhy9@gmail.com>
2026-01-06 11:06:30 +0100

    Ajout d'une classe minimale Categorie

---
+++ TpNoteCSharp/TpNoteCSharp/assets/Categorie.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TpNoteCSharp.assets
+{
+    internal class Categorie
+    {
+        private string _nom;
+        private Livre[] _livres;
+
+        public Categorie(string nom, Livre[] livres)
+        {
+            _nom = nom;
+            _livres = livres;
+        }
+
+        public string Nom
+        {
+            get { return _nom; }
+            set { _nom = value; }
+        }
+
+        public Livre[] Livres
+        {
+            get { return _livres; }
+            set { _livres = value; }
+        }
+    }
+}
