1c0cd36558658edcc9770ce58faa73004ecbb87e
Windowsrsakhy <rayansakhy9@gmail.com>
2026-01-06 11:21:09 +0100

    Add XML serialization support to Utilisateur class
    Enabled XML serialization for Utilisateur by adding serialization attributes, a parameterless constructor, and configuring property mappings for XML elements and arrays. This allows easy XML serialization and deserialization of user data.

--- TpNoteCSharp/TpNoteCSharp/assets/Utilisateur.cs
+++ TpNoteCSharp/TpNoteCSharp/assets/Utilisateur.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace TpNoteCSharp.assets
 {
+    [Serializable]
+    [XmlRoot("Utilisateur")]
     internal class Utilisateur
     {
         private string _nom;
@@ -13,6 +16,11 @@ namespace TpNoteCSharp.assets
         private string _email;
         private DateTime _dateInscription;
         private Livre[] _livresEmpruntes;
+
+        // Constructeur sans paramètres requis pour la désérialisation
+        public Utilisateur()
+        {
+        }
 
         public Utilisateur(string nom, string prenom, string email, DateTime dateInscription, Livre[] livresEmpruntes)
         {
@@ -23,30 +31,36 @@ namespace TpNoteCSharp.assets
             _livresEmpruntes = livresEmpruntes;
         }
 
+        [XmlElement("Nom")]
         public string Nom
         {
             get { return _nom; }
             set { _nom = value; }
         }
 
+        [XmlElement("Prenom")]
         public string Prenom
         {
             get { return _prenom; }
             set { _prenom = value; }
         }
 
+        [XmlElement("Email")]
         public string Email
         {
             get { return _email; }
             set { _email = value; }
         }
 
+        [XmlElement("DateInscription")]
         public DateTime DateInscription
         {
             get { return _dateInscription; }
             set { _dateInscription = value; }
         }
 
+        [XmlArray("LivresEmpruntes")]
+        [XmlArrayItem("Livre")]
         public Livre[] LivresEmpruntes
         {
             get { return _livresEmpruntes; }
