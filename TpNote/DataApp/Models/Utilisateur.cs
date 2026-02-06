a859d4284dc9158a85a0cbe672e26262434e1c42
Windowsrsakhy <rayansakhy9@gmail.com>
2026-02-06 20:19:35 +0100

    Ajout d'identifiant de connexion aux utilisateurs

--- TpNote/DataApp/Models/Utilisateur.cs
+++ TpNote/DataApp/Models/Utilisateur.cs
@@ -16,19 +16,42 @@ namespace DataApp
         private string _email;
         private DateTime _dateInscription;
         private Livre[] _livresEmpruntes;
+        private string _username;
+        private string _password;
 
         // Constructeur sans paramètres requis pour la désérialisation
         public Utilisateur()
         {
         }
 
-        public Utilisateur(string nom, string prenom, string email, DateTime dateInscription, Livre[] livresEmpruntes)
+        public Utilisateur(string username, string motDePasse, string nom, string prenom, string email, DateTime dateInscription, Livre[] livresEmpruntes)
         {
+            _username = username;
+            _password = _password;
             _nom = nom;
             _prenom = prenom;
             _email = email;
             _dateInscription = dateInscription;
             _livresEmpruntes = livresEmpruntes;
+        }
+
+        public Utilisateur(string username, string motDePasse,string nom, string prenom,string email)
+        {
+            this.Utilisateur(username, motDePasse, nom, prenom, email, DateTime.Now, new Livre[0]);
+        }
+
+        [XmlElement("Username")] 
+        public string Username 
+        { 
+            get => _username; 
+            set => _username = value; 
+        }
+        
+        [XmlElement("MotDePasse")]
+        public string MotDePasse        
+        {           
+            get => _motDePasse; 
+            set => _motDePasse = value;        
         }
 
         [XmlElement("Nom")]
