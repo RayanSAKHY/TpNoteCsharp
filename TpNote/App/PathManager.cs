ee6683ad1a9bbc5ec039708f40a92c86fc58f12a
Windowsrsakhy <rayansakhy9@gmail.com>
2026-02-06 20:00:10 +0100

    Ajout d'un path manager

---
+++ TpNote/App/PathManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace App
+{
+    internal static class PathManager
+    {
+        public static string GetLibraryFolder()
+        {
+            string root = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string folder = Path.Combine(root, "TpNoteCSharp","Bibliotheque");
+            if (!Directory.Exists(folder))
+                Directory.CreateDirectory(folder);
+            return folder;
+        }
+
+        public static string CombineWithFolder(string fileName)
+        {
+            return Path.Combine(GetLibraryFolder(), fileName);
+        }
+    }
+}
