5bd8dfbd161e7beae1845c32b92d2ccadb5d2e14
Windowsrsakhy <rayansakhy9@gmail.com>
2026-02-08 19:45:17 +0100

    Ajout d'une exception pour le cryptage non reussi

---
+++ TpNote/SerializationApp/DecryptionException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SerializationApp
+{
+    /// <summary>
+    /// Exception utilisée pour indiquer qu'un fichier existe mais n'a pas pu être déchiffré/lu.
+    /// </summary>
+    public class DecryptionException : Exception
+    {
+        /// <summary>
+        /// Crée une instance avec un message d'erreur.
+        /// </summary>
+        /// <param name="message">Description de l'erreur de déchiffrement.</param>
+        public DecryptionException(string message) : base(message) { }
+    }
+}
